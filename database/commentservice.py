60ca06580412d6a42d2481f3c2ee62256e9c838e
Javlon_mac <richk880@gmail.com>
2023-09-14 19:42:06 +0500

    весь функционал БД прописан

--- database/commentservice.py
+++ database/commentservice.py
@@ -5,16 +5,43 @@ from database.models import Comment
 
 
 # Опубликовать комментарий
-def add_comment_db(post_id, text):
-    pass
+def add_comment_db(post_id, text, user_id):
+    db = next(get_db())
+
+    new_comment = Comment(post_id=post_id, comment_text=text, user_id=user_id)
+
+    db.add(new_comment)
+    db.commit()
+
+    return "Комментарий успешно добавлен"
 
 
 # Удалить комментарий
 def delete_comment_db(comment_id):
-    pass
+    db = next(get_db())
+
+    exact_post_comment = db.query(Comment).filter_by(id=comment_id).first()
+
+    if exact_post_comment:
+        db.delete(exact_post_comment)
+        db.commit()
+
+        return "успешно удален"
+
+    return False
 
 
 # Изменить определенный комментарий
 def change_comment_db(comment_id, new_comment):
-    pass
+    db = next(get_db())
+
+    exact_post_comment = db.query(Comment).filter_by(id=comment_id).first()
+
+    if exact_post_comment:
+        exact_post_comment.comment_text = new_comment
+        db.commit()
+
+        return "Комментарий успешно изменен"
+
+    return False
 
